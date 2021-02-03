f699a65ac7e2ca2ef3164a993f60eef95faa1a55
legabog <probringer@mail.ru>
2021-02-03 21:29:56 +0300

    added planets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Canvas } from "react-three-fiber";
-import { OrbitControls, Sky } from "@react-three/drei";
+import { OrbitControls, Stars, useGLTF } from "@react-three/drei";
 import "./App.css";
 import Ground from "./components/Ground/Ground";
 
 function App(props) {
   return (
-    <Canvas>
-      <Sky sunPosition={[100, 20, 100]} />
+    <Canvas args={[400, 400]}>
+      <Stars />
       <ambientLight intensity={0.5} />
       <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
       <Suspense fallback={null}>
--- src/components/Ground/Ground.js
+++ src/components/Ground/Ground.js
@@ -1,10 +1,65 @@
+import * as THREE from "three";
 import { useThree } from "react-three-fiber";
-import { useTexture } from "@react-three/drei";
-import grass_texture from "../../assests/textures/grass-texture.jpg";
+import {
+  Box,
+  ScreenQuad,
+  PerspectiveCamera,
+  useTexture,
+  RoundedBox,
+  Sphere,
+} from "@react-three/drei";
+import ground_stone from "../../assests/textures/ground_stone/ground_stone.jpg";
+import ground_stone_normal from "../../assests/textures/ground_stone/ground_stone_normal.jpg";
+import ground_stone_height from "../../assests/textures/ground_stone/ground_stone_height.png";
+
+import evil_planet_color from "../../assests/textures/evil_planet/evil_planet_color.png";
+import evil_planet_disp from "../../assests/textures/evil_planet/evil_planet_disp.png";
+import evil_planet_normal from "../../assests/textures/evil_planet/evil_planet_normal.png";
+import evil_planet_spec from "../../assests/textures/evil_planet/evil_planet_spec.png";
+import evil_planet_occ from "../../assests/textures/evil_planet/evil_planet_occ.png";
+
+import angel_planet_color from "../../assests/textures/angel_planet/angel_planet_color.jpg";
+import angel_planet_disp from "../../assests/textures/angel_planet/angel_planet_disp.png";
+import angel_planet_normal from "../../assests/textures/angel_planet/angel_planet_normal.jpg";
+import angel_planet_spec from "../../assests/textures/angel_planet/angel_planet_spec.jpg";
+import angel_planet_ao from "../../assests/textures/angel_planet/angel_planet_ao.jpg";
 
 const Ground = (props) => {
+  // const [bumpMap, specMap, normalMap] = useLoader(TextureLoader, [url1, url2, url2])
   // const { camera } = useThree();
-  const texture = useTexture(grass_texture);
+  const [texture1, texture2, dispTexture] = useTexture([
+    ground_stone,
+    ground_stone_normal,
+    ground_stone_height,
+  ]);
+
+  const [
+    evilPlanetColor,
+    evilPlanetDisp,
+    evilPlanetNormal,
+    evilPlanetSpec,
+    evilPlanetOcc,
+  ] = useTexture([
+    evil_planet_color,
+    evil_planet_disp,
+    evil_planet_normal,
+    evil_planet_spec,
+    evil_planet_occ,
+  ]);
+
+  const [
+    angelPlanetColor,
+    angelPlanetDisp,
+    angelPlanetNormal,
+   angelPlanetSpec,
+    angelPlanetAo,
+  ] = useTexture([
+    angel_planet_color,
+    angel_planet_disp,
+    angel_planet_normal,
+    angel_planet_spec,
+    angel_planet_ao,
+  ]);
 
   // camera.fov = 20;
   // camera.aspect = window.innerWidth / window.innerHeight;
@@ -15,10 +70,37 @@ const Ground = (props) => {
   // camera.position.set(100, 100, 100);
 
   return (
-    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshBasicMaterial attach="material" map={texture} />
-    </mesh>
+    <group>
+      <Sphere position={[10, 5, 0]}>
+        <sphereGeometry args={[4, 32, 32]} />
+        <meshPhongMaterial
+          map={evilPlanetColor}
+          normalMap={evilPlanetNormal}
+          bumpMap={evilPlanetDisp}
+          bumpScale={1.5}
+          specularMap={evilPlanetSpec}
+          aoMap={evilPlanetOcc}
+        />
+        <ambientLight intensity={2} />
+      </Sphere>
+
+      <Sphere position={[-10, 5, 0]}>
+        <sphereGeometry args={[4, 32, 32]} />
+        <meshPhongMaterial
+          map={angelPlanetColor}
+          normalMap={angelPlanetNormal}
+          bumpMap={angelPlanetDisp}
+          bumpScale={2.5}
+          specularMap={angelPlanetSpec}
+          alphaMap={angelPlanetAo}
+        />
+      </Sphere>
+
+      <Box args={[400, 400, 40]} position={[0, 0, 0]}>
+        <meshPhongMaterial map={texture1} bumpMap={dispTexture} bumpScale={5.5}/>
+        <cylinderGeometry height={40} />
+      </Box>
+    </group>
   );
 };
 
